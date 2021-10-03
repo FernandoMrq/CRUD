11f1cfa9be442f83ba268eb64e619a806b118ca8
Fernando Marques de Oliveira <frndoliveira@gmail.com>
2021-10-03 18:43:29 -0300

    Change connection string and others.

---
+++ Models/ApiContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace CRUD.Models
+{
+    public partial class ApiContext : DbContext
+    {
+        public ApiContext()
+        {
+        }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("ConnectionStringPrincipal");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
