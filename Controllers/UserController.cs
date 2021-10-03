23e3206af35a3cbe55c28df15fc12e98c4871e2b
Fernando Marques de Oliveira <frndoliveira@gmail.com>
2021-10-03 20:22:38 -0300

    Controller Finalizado e outros.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,7 @@
 ﻿using CRUD.Models;
 using CRUD.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -27,6 +27,66 @@ namespace CRUD.Controllers
                 return BadRequest();
             }
             return Ok(users.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await repository.GetById(id);
+
+            if (user == null)
+            {
+                return NotFound("Usuario não encontrado pelo id informado");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Usuário inválido");
+            }
+
+            await repository.Insert(user);
+
+            return CreatedAtAction(nameof(GetUser), new { Id = user.Id }, user);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutUser(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Usuário inválido");
+            }
+
+            try
+            {
+                await repository.Update(user);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            return Ok("Atualização do usuário realizada com sucesso");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(int id)
+        {
+            var user = await repository.GetById(id);
+            if (user == null)
+            {
+                return NotFound($"Usuário de {id} não encontrado");
+            }
+
+            await repository.Delete(id);
+
+            return Ok(user);
         }
     }
 }
